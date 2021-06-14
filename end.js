8f446d4566d8c6bfc67abdfab7a8683976a2ab30
MADHUMANTI SAMANTA <madhumantisamanta@gmail.com>
2021-06-14 19:18:15 +0530

    updated

--- end.js
+++ end.js
@@ -32,6 +32,6 @@ saveHighScore = e => {
     highScores.splice(5);
 
     localStorage.setItem("highScores", JSON.stringify(highScores));
-    window.location.assign("/");
+    window.location.assign("index.html");
     // console.log(highScores);
 };
